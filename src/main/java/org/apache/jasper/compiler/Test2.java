ff7f53e7b5b00c3375474b6e0779257752914580
quyixiao <2621048238@qq.com>
2022-09-18 11:47:02 +0800

    提交修改

--- src/main/java/org/apache/jasper/compiler/Test2.java
+++ src/main/java/org/apache/jasper/compiler/Test2.java
@@ -7,14 +7,18 @@ public class Test2 {
         JspReader reader = new JspReader("/Users/quyixiao/Desktop/testfile/zz.txt", "UTF-8", new ErrorDispatcher());
 
 
-        String str = "<%@ page contentType=\"text/html;charset=UTF-8\" language=\"java\" %>";
-        int index = str.indexOf("\"");
-        System.out.println(index);
-
         Mark mark0 = reader.mark();
         Mark mark = reader.skipUntil("\"");
         System.out.println(mark);
         System.out.println(reader.getText(mark0,mark));
+
+
+
+
+
+        String str = "<%@ page contentType=\"text/html;charset=UTF-8\" language=\"java\" %>";
+        int index = str.indexOf("\"");
+        System.out.println(index);
 
 
     }
